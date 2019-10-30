73f3ef0a8ba7468ce9747bfae28ee62036ae399a
Naomi Campos <naomicampos@nkcampos.local>
2019-10-30 15:40:09 -0600

    Add basic styling to login splash page

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ import $ from 'jquery';
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
 
 console.log('This is the JavaScript entry file - your code begins here.');
+
+// $('.login-screen').hide();
+$('.customer-view').hide();
+$('.manager-view').hide();
